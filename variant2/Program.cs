f2146c3d645a807bcca5866a1be7d28a38d7e54d
Гульназик <@LAPTOP-58TJUTTA>
2021-11-25 19:40:37 +0500

    Добавьте файлы проекта.

---
+++ variant2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace variant2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число N");
+            double val = Convert.ToDouble (Console.ReadLine());
+            // int val = 9;
+            for (int i = 1; i <= 2 * val - 1; i++)
+
+                Console.WriteLine(i + ": "+ (i*i));
+            Console.ReadKey(true);
+        }
+    }
+}
